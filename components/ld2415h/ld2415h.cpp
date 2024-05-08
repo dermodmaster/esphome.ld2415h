8e4eb15c52cea9a12432a7bc68a53c6d71fd19de
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-07 23:46:36 -0700

    Update ld2415h.cpp

--- components/ld2415h/ld2415h.cpp
+++ components/ld2415h/ld2415h.cpp
@@ -13,7 +13,19 @@ static const uint8_t LD2415H_CONFIG_REQUEST[] = {0x43, 0x46, 0x07, 0x00, 0x00, 0
 void LD2415HComponent::setup() {
   // because this implementation is currently rx-only, there is nothing to setup
 
-  this->write_byte(LD2415H_CONFIG_REQUEST);
+  this->write_byte(LD2415H_CONFIG_REQUEST[0]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[1]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[2]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[3]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[4]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[5]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[6]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[7]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[8]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[9]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[10]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[11]);
+  this->write_byte(LD2415H_CONFIG_REQUEST[12]);
 }
 
 void LD2415HComponent::update() {
