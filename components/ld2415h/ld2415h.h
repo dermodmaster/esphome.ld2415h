a642692e87b572e8947b4195f867127c64b3e578
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-10 16:07:04 -0700

    Update ld2415h.h

--- components/ld2415h/ld2415h.h
+++ components/ld2415h/ld2415h.h
@@ -79,6 +79,14 @@ class LD2415HComponent : public Component, public uart::UARTDevice {
   void parse_firmware_();
   void parse_velocity_();
   void parse_config_param_(char* key, char* value);
+
+  TrackingMode i_to_TrackingMode_(uint8_t value);
+  char* TrackingMode_to_s_(TrackingMode value);
+  UnitOfMeasure i_to_UnitOfMeasure_(uint8_t value);
+  char* UnitOfMeasure_to_s_(UnitOfMeasure value);
+  NegotiationMode i_to_NegotiationMode_(uint8_t value);
+  char* NegotiationMode_to_s_(NegotiationMode value);
+
 };
 
 }  // namespace ld2415h
