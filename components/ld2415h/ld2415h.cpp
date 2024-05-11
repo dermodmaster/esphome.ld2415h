899ca26fe6adc07c59f929b65f486a2d55fe3e40
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-11 04:27:04 -0700

    Update ld2415h.cpp

--- components/ld2415h/ld2415h.cpp
+++ components/ld2415h/ld2415h.cpp
@@ -40,13 +40,13 @@ void LD2415HComponent::dump_config() {
   uint8_t cmd[sizeof(LD2415H_CMD_SET_SPEED_ANGLE_SENSE)];
   std::memcpy(cmd, LD2415H_CMD_SET_SPEED_ANGLE_SENSE, sizeof(LD2415H_CMD_SET_SPEED_ANGLE_SENSE));
 
-  ESP_LOGD(TAG, "LD2415H_CMD_SET_SPEED_ANGLE_SENSE: %s", String(cmd));
+  ESP_LOGD(TAG, "LD2415H_CMD_SET_SPEED_ANGLE_SENSE: %s", std::String(cmd));
 
   cmd[3] = this->min_speed_threshold_;
   cmd[4] = this->compensation_angle_;
   cmd[5] = this->sensitivity_;
 
-  ESP_LOGD(TAG, "LD2415H_CMD_SET_SPEED_ANGLE_SENSE: %s", String(cmd));
+  ESP_LOGD(TAG, "LD2415H_CMD_SET_SPEED_ANGLE_SENSE: %s", std::String(cmd));
 
 }
 
