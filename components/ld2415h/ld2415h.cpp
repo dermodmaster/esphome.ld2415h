b4ced716ae340b5edf74d90d809bed8fe606757b
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-11 13:34:24 -0700

    Update ld2415h.cpp

--- components/ld2415h/ld2415h.cpp
+++ components/ld2415h/ld2415h.cpp
@@ -58,7 +58,7 @@ void LD2415HComponent::set_min_speed_threshold(uint8_t speed) {
   cmd[5] = this->sensitivity_;
 
   ESP_LOGD(TAG, "LD2415H_CMD_SET_SPEED_ANGLE_SENSE: ");
-  //this->issue_command_(cmd, sizeof(cmd));
+  this->issue_command_(cmd, sizeof(cmd));
 };
 
 void LD2415HComponent::set_compensation_angle(uint8_t angle) {
