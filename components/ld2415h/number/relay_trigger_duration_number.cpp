7704ff1de63ecc7d489a9ba06206bfeb2f267bc1
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-26 21:48:35 -0700

    Update relay_trigger_duration_number.cpp

--- components/ld2415h/number/relay_trigger_duration_number.cpp
+++ components/ld2415h/number/relay_trigger_duration_number.cpp
@@ -7,7 +7,7 @@ RelayTriggerDurationNumber::RelayTriggerDurationNumber(uint8_t duration) : durat
 
 void RelayTriggerDurationNumber::control(uint8_t duration) {
   this->publish_state(duration);
-  this->parent_->set_min_reported_duration(this->duration_);
+  this->parent_->set_relay_trigger_duration(this->duration_);
 }
 
 }  // namespace ld2415h
