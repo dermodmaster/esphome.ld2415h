79db3b55c6abd961e4f20bdf351f0f3c561dc25b
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-27 02:02:23 -0700

    Renamed sensor

--- components/ld2415h/binary_sensor/__init__.py
+++ components/ld2415h/binary_sensor/__init__.py
@@ -7,16 +7,18 @@ from esphome.const import (
 )
 from .. import ld2415h_ns, LD2415HComponent, CONF_LD2415H_ID
 
-LD2415HBinarySensor = ld2415h_ns.class_(
-    "LD2415HBinarySensor", binary_sensor.BinarySensor, cg.Component
+ApproachingBinarySensor = ld2415h_ns.class_(
+    "ApproachingBinarySensor", binary_sensor.BinarySensor
 )
 
 CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA.extend(
         {
-            cv.GenerateID(): cv.declare_id(LD2415HBinarySensor),
+            cv.GenerateID(): cv.declare_id(ApproachingBinarySensor),
             cv.GenerateID(CONF_LD2415H_ID): cv.use_id(LD2415HComponent),
-            cv.Optional(CONF_BINARY_SENSOR): binary_sensor.binary_sensor_schema(),
+            cv.Optional(CONF_BINARY_SENSOR): binary_sensor.binary_sensor_schema(
+                ApproachingBinarySensor,
+            ),
         }
     ),
 )
--- components/ld2415h/binary_sensor/approaching_binary_sensor.cpp
+++ components/ld2415h/binary_sensor/approaching_binary_sensor.cpp
@@ -1,14 +1,14 @@
-#include "ld2415h_binary_sensor.h"
+#include "approaching_binary_sensor.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
 namespace ld2415h {
 
-static const char *const TAG = "LD2415H.binary_sensor";
+static const char *const TAG = "Approaching.binary_sensor";
 
-void LD2415HSensor::dump_config() {
-  ESP_LOGCONFIG(TAG, "LD2415H BinarySensor:");
+void ApproachingBinarySensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "Approaching BinarySensor:");
   LOG_SENSOR("  ", "Approaching", this->approaching_binary_sensor_);
 }
 
