cdac6dcc2df774b3ac0a75a711e7baf470486575
CptSkippy <cptskippy@users.noreply.github.com>
2024-05-31 16:42:27 -0700

    Update __init__.py

--- components/ld2415h/binary_sensor/__init__.py
+++ components/ld2415h/binary_sensor/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
-from esphome.const import CONF_ID, CONF_BINARY_SENSOR, DEVICE_CLASS_MOVING
+from esphome.const import CONF_ID, DEVICE_CLASS_MOVING
 from .. import ld2415h_ns, LD2415HComponent, CONF_LD2415H_ID
 
 LD2415HBinarySensor = ld2415h_ns.class_(
@@ -10,12 +10,14 @@ LD2415HBinarySensor = ld2415h_ns.class_(
 
 ICON_APPROACHING = "mdi:arrow-left-right"
 
+CONF_APPROACHING = "approaching"
+
 CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(LD2415HBinarySensor),
             cv.GenerateID(CONF_LD2415H_ID): cv.use_id(LD2415HComponent),
-            cv.Optional(CONF_BINARY_SENSOR): binary_sensor.binary_sensor_schema(
+            cv.Optional(CONF_APPROACHING): binary_sensor.binary_sensor_schema(
                 device_class=DEVICE_CLASS_MOVING,
                 icon=ICON_APPROACHING,
             ),
@@ -27,7 +29,7 @@ CONFIG_SCHEMA = cv.All(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    if approaching := config.get(CONF_BINARY_SENSOR):
+    if approaching := config.get(CONF_APPROACHING):
         sens = await binary_sensor.new_binary_sensor(approaching)
         cg.add(var.set_approaching_sensor(sens))
     ld2415h = await cg.get_variable(config[CONF_LD2415H_ID])
