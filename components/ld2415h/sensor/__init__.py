c3f83abd3ed1bf43112dbb89226153b5c98b46b0
CptSkippy <cptskippy@users.noreply.github.com>
2024-06-01 16:47:02 -0700

    Update __init__.py

--- components/ld2415h/sensor/__init__.py
+++ components/ld2415h/sensor/__init__.py
@@ -43,17 +43,15 @@ CONFIG_SCHEMA = cv.All(
 
 
 async def to_code(config):
-    ld2415h = await cg.get_variable(config[CONF_LD2415H_ID])
-
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-
+    ld2415h_component = await cg.get_variable(config[CONF_LD2415H_ID])
     if speed := config.get(CONF_SPEED):
         sens = await sensor.new_sensor(speed)
-        cg.add(var.set_speed_sensor(sens))
-        
+        await cg.register_parented(sens, config[CONF_LD2415H_ID])
+        cg.add(ld2415h_component.set_speed_sensor(sens))
+
     if velocity := config.get(CONF_VELOCITY):
         sens = await sensor.new_sensor(velocity)
-        cg.add(var.set_velocity_sensor(sens))
-        
-    cg.add(ld2415h.register_listener(var))
+        await cg.register_parented(sens, config[CONF_LD2415H_ID])
+        cg.add(ld2415h_component.set_speed_sensor(sens))
+    var = cg.new_Pvariable(config[CONF_ID])
+    cg.add(ld2415h_component.register_listener(var))
